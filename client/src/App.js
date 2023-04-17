dad0a86fba3fa51aa3d4f1a0147f3f1426683f7c
shch989 <tankpop11@gmail.com>
2023-04-18 00:31:42 +0900

    Building homepage with react

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,83 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <main>
+      <header>
+        <a href="/" className="logo">
+          MyBlog
+        </a>
+        <nav>
+          <a href="/">Login</a>
+          <a href="/">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=730&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a href="/" className="author">
+              Seong-Hyeon JO
+            </a>
+            <time>2023-04-18 00:22</time>
+          </p>
+          <p className="summary">
+            Today at its special launch event, home backup power giant EcoFlow
+            launched a flurry of new products, including a “Whole-Home Backup
+            Power Solution.”
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=730&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a href="/" className="author">
+              Seong-Hyeon JO
+            </a>
+            <time>2023-04-18 00:22</time>
+          </p>
+          <p className="summary">
+            Today at its special launch event, home backup power giant EcoFlow
+            launched a flurry of new products, including a “Whole-Home Backup
+            Power Solution.”
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=730&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a href="/" className="author">
+              Seong-Hyeon JO
+            </a>
+            <time>2023-04-18 00:22</time>
+          </p>
+          <p className="summary">
+            Today at its special launch event, home backup power giant EcoFlow
+            launched a flurry of new products, including a “Whole-Home Backup
+            Power Solution.”
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
 
