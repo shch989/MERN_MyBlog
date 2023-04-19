0e57e5fe9edab339d2484b75e656b12b29acf0ad
shch989 <tankpop11@gmail.com>
2023-04-19 22:58:34 +0900

    Login and Register Page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
       </Link>
       <nav>
         <Link to="/login">Login</Link>
-        <Link to="/">Register</Link>
+        <Link to="/register">Register</Link>
       </nav>
     </header>
   )
